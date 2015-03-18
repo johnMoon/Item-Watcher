43c89e029a275084536cb3ee57fb0299b9c9d30b
David Dang <daviddang.92@gmail.com>
2015-03-18 00:18:14 -0700

    merged search functionality to search window.

--- Files/mainWindowSearch.js
+++ Files/mainWindowSearch.js
@@ -1,65 +1,43 @@
 
 
 
-
-
-function findMatchingItem(){
+function findMatchingItem() {
 	var searchItem = document.getElementById("search").value.trim();
-	
+
 	console.log(searchItem);
-	
-	if(searchItem){
-		var spidy = "http://www.gw2spidy.com/api/v0.9/json/item-search/"+searchItem+"?callback=?";
-	
-		$.getJSON(spidy,function(data){
+
+	if (searchItem) {
+		var spidy = "http://www.gw2spidy.com/api/v0.9/json/item-search/" + searchItem + "?callback=?";
+
+		$.getJSON(spidy, function (data) {
 			console.log(data.results);
 
+		}).done(function (data) {
+			console.log(!data.result);
+			if (!data.result) {
+				openSubWindow();
 
+			}
 
-		}).done(function(data){
-		if(!data.result){
-			openSubWindow();
-		
-		}
-			
-		$.each(data.results,function(i,item){
-			if(item.name == searchItem){
-			
-			console.log("There is a matching Item");
-			} 
-			
-			
-			
-			
-			
-		
-		
-		
+			$.each(data.results, function (i, item) {
+				if (item.name == searchItem) {
+
+					console.log("There is a matching Item");
+				}
+
+			});
+
 		});
-		
-		
-		
-		
-	
+	}
+}
 
+function openSubWindow() {
+	//alert("the subwindow will only be visible inside a game");
+	overwolf.windows.obtainDeclaredWindow("SubWindow", function (result) {
+		if (result.status == "success") {
+			overwolf.windows.restore(result.window.id, function (result) {
+				console.log(result);
+			});
+		}
 	});
-	
-
-	}}
-
-
-
-function openSubWindow(){
-				//alert("the subwindow will only be visible inside a game");
-				overwolf.windows.obtainDeclaredWindow("SubWindow", function(result){
-					if (result.status == "success"){
-						overwolf.windows.restore(result.window.id, function(result){
-								console.log(result);
-						});
-					}
-				});
-			};
-
-
-
-
+};
