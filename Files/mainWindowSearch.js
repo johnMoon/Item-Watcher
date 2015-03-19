8c2ba5c9a93fb7db4bb75115570bef2a02117ab1
David Dang <daviddang.92@gmail.com>
2015-03-19 13:38:28 -0700

    fixed window search js merging

--- Files/mainWindowSearch.js
+++ Files/mainWindowSearch.js
@@ -1,77 +1,34 @@
-
-
-
 function findMatchingItem() {
-	var searchItem = document.getElementById("search").value.trim();
+    var searchItem = document.getElementById("search").value.trim();
 
-	console.log(searchItem);
+    console.log(searchItem);
 
-	if (searchItem) {
-		var spidy = "http://www.gw2spidy.com/api/v0.9/json/item-search/" + searchItem + "?callback=?";
+    if (searchItem) {
+        var spidy = "http://www.gw2spidy.com/api/v0.9/json/item-search/" + searchItem + "?callback=?";
 
-<<<<<<< HEAD
-		}).done(function(data){
-		if(data.total != 1){
-			openSubWindow();
-		
-		}
-			
-		$.each(data.results,function(i,item){
-			if(item.name == searchItem){
-			
-			console.log("There is a matching Item");
-			} 
-			
-			
-			
-			
-			
-		
-		
-		
-		});
-		
-		
-		
-		
-	
 
-	});
-	
+        $.getJSON(spidy).done(function(data) {
+            if (data.total != 1) {
+                openSubWindow();
+            }
+            $.each(data.results, function(i, item) {
+                if (item.name == searchItem) {
 
-	}}
+                    console.log("There is a matching Item");
+                }
+            });
+        });
 
-=======
-		$.getJSON(spidy, function (data) {
-			console.log(data.results);
->>>>>>> f9693f9bb418f581e48e196da9ea9ddd620c14b9
-
-		}).done(function (data) {
-			console.log(!data.result);
-			if (!data.result) {
-				openSubWindow();
-
-			}
-
-			$.each(data.results, function (i, item) {
-				if (item.name == searchItem) {
-
-					console.log("There is a matching Item");
-				}
-
-			});
-
-		});
-	}
+    }
 }
 
 function openSubWindow() {
-	//alert("the subwindow will only be visible inside a game");
-	overwolf.windows.obtainDeclaredWindow("SubWindow", function (result) {
-		if (result.status == "success") {
-			overwolf.windows.restore(result.window.id, function (result) {
-				console.log(result);
-			});
-		}
-	});
+    //alert("the subwindow will only be visible inside a game");
+    overwolf.windows.obtainDeclaredWindow("SubWindow", function(result) {
+        if (result.status == "success") {
+            overwolf.windows.restore(result.window.id, function(result) {
+                console.log(result);
+            });
+        }
+    });
 };
