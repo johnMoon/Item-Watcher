71621ec5a673cb0875e71a63c311d39f6960b485
John Moon <Jmoon2083@gmail.com>
2015-03-21 23:11:53 -0700

    table-> div finished, added some styles to watchitems

--- Files/mainWindowSearch.js
+++ Files/mainWindowSearch.js
@@ -116,91 +116,65 @@ function updateItemPrices() {
 function createSearchItem(itemId, name, image, bPrice, sPrice) {
 
     //var li = $(document.createElement('li'));
-    var table = $(document.createElement('table'));
-    $(table).addClass("watch-header");
-    var img = $(document.createElement('img')).attr('src', image);
-    $(img).attr('height', "32").attr('width', "32");
-    $(img).addClass("item-img");
-    var span = $(document.createElement('span')).text(name);
-    span.addClass("item-name");
-    var button = $(document.createElement('button')).text("X");
-    button.addClass("right-button");
+	var div = $(document.createElement('div'));	
+		div.addClass('mainWindow-item-cell');
+	var img = $(document.createElement('img')).attr('src', image);
+	 $(img).attr('height', "32").attr('width',"32");
+	 $(img).addClass("item-img");
+	var p = $(document.createElement('p')).text(name);
+		p.addClass('item-name');
+	
+	 var button = $(document.createElement('button')).text("X");
 
-    var tr = document.createElement('tr');
-    var td = document.createElement('td');
-    var td2 = document.createElement('td');
-    var td3 = document.createElement('td');
+	 
+	 var div2 = $(document.createElement('div'));
+	 div2.addClass('graph-buysell');
+	
+		
+	 var img2 = $(document.createElement('img')).attr('src','https://www.google.com/jsapi?autoload={%20&#39;modules&#39;:[{%20&#39;name&#39;:&#39;visualization&#39;,%20&#39;version&#39;:&#39;1&#39;,%20&#39;packages&#39;:[&#39;corechart&#39;]%20}]%20}');
+		$(img2).attr('width',"180x").attr('height',"32px");
+	 var span = $(document.createElement('span'));
+	 span.attr('width',"170px");
+	 
+		
+		
+		span.append($(img2));
+		
+	 div2.append(span);
+	 
+	 var div3 = $(document.createElement('div'));
+	 div3.addClass('buy-sell-prices');
+	 
+	 var p2 =$(document.createElement('p')).text("Buy: ");
+		 p2.addClass('buy-sell');
+	 var span2 = $(document.createElement('span')).text(bPrice);
+		 $(span2).attr("id", itemId +"-buy");
+		 p2.append(span2);
+	 
+	var p3 = $(document.createElement('p')).text("Sell: "); 
+		p3.addClass('buy-sell');
+	var span3 = $(document.createElement('span')).text(sPrice);
+		$(span3).attr("id", itemId +"-sell");
+		p3.append(span3);
+		
+		div3.append(p2);
+		div3.append(p3);
+		
+		div2.append(div3);
+	
+	var result = div.append($(img));
+	result.append(p);
+	result.append($(button));
+	result.append(div2);
+	//result.append(p2);
+	//result.append(p3);
 
-    $(td3).append($(button));
-    $(td).append($(img));
-    $(td2).append(span);
-    $(tr).append($(td));
-    $(tr).append($(td2));
-    $(tr).append($(td3));
-
-    var result = $(table).append($(tr));
-
-
-
-
-    var table2 = $(document.createElement('table'));
-    $(table).addClass("watch-data");
-    var tr2 = $(document.createElement('tr'));
-    var td4 = $(document.createElement('td'));
-    tr2.append($(td4));
-
-    table2.append($(tr2));
-
-    var tr3 = $(document.createElement('tr'));
-    var td5 = $(document.createElement('td'));
-
-    $(td5).attr('rowspan', "2").attr('align', "center").addClass("graph");
-    var div = $(document.createElement('div'));
-
-    $(div).attr('id', "curve_chart").attr('style', "width: 180x; height: 32px");
-    td5.append($(div));
-    tr3.append($(td5));
-
-    var td6 = $(document.createElement('td'));
-    $(td6).addClass("buy")
-    td6.append("buy");
-
-    tr3.append($(td6));
-
-    var td7 = $(document.createElement('td'));
-    $(td7).addClass("buying-price");
-    var span2 = $(document.createElement('span')).text(bPrice);
-    $(span2).attr("id", itemId +"-buy");
-    td7.append(span2);
-
-    tr3.append($(td7));
-
-    table2.append($(tr3));
-
-
-    var tr4 = $(document.createElement('tr'));
-
-    var td8 = $(document.createElement('td'));
-    $(td8).addClass("sell")
-    td8.append("sell");
-
-    tr4.append($(td8));
-
-    var td9 = $(document.createElement('td'));
-    $(td9).addClass("Selling-price");
-    var span3 = $(document.createElement('span')).text(sPrice);
-    $(span3).attr("id", itemId +"-sell");
-
-    td9.append(span3);
-
-    tr4.append(td9);
-
-    table2.append($(tr4));
-
-    result.append($(table2));
-    $(result).appendTo("#testItem");
-
-
+	
+	
+	
+	
+	 $(result).appendTo("#testItem");
+   
 }
 
 
